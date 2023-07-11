f8bbcbe839b2549a7609a6e59540fe71ba2c85fa
GalangRmd <139119220+GalangRmd@users.noreply.github.com>
2023-07-11 11:48:28 +0700

    Add files via upload

---
+++ day4(Function).js
@@ -0,0 +1,24 @@
+//memngembalikan Nama kalian
+function namakalian() {
+const nama='galang';
+console.log('Nama : '+nama)
+}
+namakalian();
+
+//perkalian
+
+function kaliinweh(x, y) {
+return x * y;
+}
+let hasilkali= kaliinweh(5, 5);
+console.log('X X Z '+ hasilkali)
+
+//biodata
+function data_diri() {
+let nama='galang';
+let umur= '17 tahun';
+let alamat = 'bandung';
+let hobby = 'bermain game';
+console.log('Nama saya '+ nama + 'dan Umur '+ umur + 'saya tinggal di '+ alamat+ ' dan saya memiliki hobby ' + hobby)
+}
+data_diri()
